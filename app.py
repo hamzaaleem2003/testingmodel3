1a6f6a73ef00fd87029be26d0bcd58bc821e68ac
hamzaaleem2003 <125291397+hamzaaleem2003@users.noreply.github.com>
2024-08-17 02:03:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -63,17 +63,18 @@ class ChatBot():
 
 # Create an instance of the ChatBot class
 bot = ChatBot()
+# input = input("Ask me anything: ")
+# result = bot.rag_chain.invoke(input)
+# print(result)
+
 st.set_page_config(page_title="Urdu Book Bot")
 with st.sidebar:
     st.title('Urdu Book Bot')
 
-# Function for generating LLM response incrementally
-def generate_response_stream(input):
-    response = bot.rag_chain.invoke(input)
-    # Simulate streaming by yielding one character at a time
-    for char in response:
-        yield char
-        time.sleep(0.005)  # Adjust this to control the typing speed
+# Function for generating LLM response
+def generate_response(input):
+    result = bot.rag_chain.invoke(input)
+    return result
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -93,13 +94,10 @@ if input := st.chat_input():
 # Generate a new response if the last message is not from the assistant
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
-        response_container = st.empty()  # Create an empty container for streaming the response
-        response_text = ""
-
-        for char in generate_response_stream(input):
-            response_text += char
-            response_container.write(response_text)
-
-    message = {"role": "assistant", "content": response_text}
+            
+        # with st.spinner("Getting your answer from mystery stuff.."):
+            response = generate_response(input)
+            st.write(response)
+    message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
 
